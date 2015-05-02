13e78a5b4e1dbf8f41330aafe4f012160e54d84a
Colin Sherratt <colin.sherratt@gmail.com>
2015-05-02 04:04:33 -0400

    Use callbacks

--- src/back.rs
+++ src/back.rs
@@ -9,35 +9,10 @@ use pulse::*;
 
 use {Handle, Wait};
 
-
-struct Pending {
-    task: Box<FnBox() + Send>,
-    trigger: Pulse,
-    done: Signal
-}
-
 struct Inner {
     exit: Option<Pulse>,
     exit_method: Wait,
-    pending: SelectMap<Pending>,
     active: SelectMap<thread::JoinHandle<()>>
-}
-
-impl Inner {
-    fn launch(&mut self, pending: Pending) {
-        let Pending {
-            task,
-            trigger,
-            done: p
-        } = pending;
-
-        let thread = thread::spawn(move|| {
-            (task)();
-            trigger.pulse();
-        });
-
-        self.active.add(p, thread);
-    }
 }
 
 /// Task queue back-end.
@@ -52,19 +27,13 @@ impl Backend {
             inner: Mutex::new(Inner{
                 exit: None,
                 exit_method: Wait::None,
-                pending: SelectMap::new(),
                 active: SelectMap::new(),
             })
         }
     }
 
     pub fn start(&self, mut deps: Vec<Handle>, task: Box<FnBox() + Send>) -> Handle {
-        let (p, t) = Signal::new();
-        let pending = Pending {
-            task: task,
-            done: p.clone(),
-            trigger: t
-        };
+        let (signal, complete) = Signal::new();
 
         let pulse = if deps.len() == 0 {
             let (pulse, t) = Signal::new();
@@ -78,13 +47,13 @@ impl Backend {
             barrier.signal()
         };
 
-        let mut guard = self.inner.lock().unwrap();
-        if pulse.is_pending() {
-            guard.pending.add(pulse, pending);
-        } else {
-            guard.launch(pending);
-        }
-        p
+        pulse.callback(move || {
+            thread::spawn(move || {
+                task();
+                complete.pulse();
+            });
+        });
+        signal
     }
 
     pub fn exit(&self, wait: Wait) {
@@ -98,11 +67,10 @@ impl Backend {
         let (exit_p, exit) = Signal::new();
         let mut select = Select::new();
         let exit_id = select.add(exit_p);
-        let (mut pending_id, mut active_id) = {
+        let mut active_id = {
             let mut guard = self.inner.lock().unwrap();
             guard.exit = Some(exit);
-            (select.add(guard.pending.signal()),
-             select.add(guard.active.signal()))
+            select.add(guard.active.signal())
         };
 
         // Tell the caller that we have started
@@ -112,14 +80,7 @@ impl Backend {
         while let Some(pulse) = select.next() {
             let mut guard = self.inner.lock().unwrap();
 
-            if pulse.id() == pending_id {
-                pending_id = select.add(guard.pending.signal());
-                if let Some((_, task)) = guard.pending.try_next() {
-                    if exit_method != Some(Wait::Active) {
-                        guard.launch(task);
-                    }
-                }
-            } else if pulse.id() == active_id {
+            if pulse.id() == active_id {
                 active_id = select.add(guard.active.signal());
                 if let Some((_, task)) = guard.active.try_next() {
                     task.join().unwrap();
@@ -128,10 +89,10 @@ impl Backend {
                 exit_method = Some(guard.exit_method);
             }
 
-            match (exit_method, guard.pending.len(), guard.active.len()) {
-                (Some(Wait::None), _, _) => break,
-                (Some(Wait::Active), _, 0) => break,
-                (Some(Wait::Pending), 0, 0) => break,
+            match (exit_method, guard.active.len()) {
+                (Some(Wait::None), _) => break,
+                (Some(Wait::Active), 0) => break,
+                (Some(Wait::Pending), 0) => break,
                 _ => ()
             }
         }
